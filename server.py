502143480d453abaa199fe529e9d217283cd87d8
Vladimir Grebenev <grebenev-81@mail.ru>
2023-04-24 09:21:25 +0300

    not work

--- server.py
+++ server.py
@@ -3,9 +3,13 @@
 import socket
 import sys
 import json
+import time
+
+import select
+
 from common_files.settings import ACTION, ACCOUNT_NAME, RESPONSE, \
     MAX_CONNECTIONS, PRESENCE, TIME, USER, ERROR, DEFAULT_PORT, \
-    DEFAULT_IP_ADDRESS
+    DEFAULT_IP_ADDRESS, MESSAGE, MESSAGE_TEXT, SENDER
 from common_files.plugins import get_msg, send_msg
 import logging
 import log.server_log_config
@@ -13,22 +17,28 @@ from log.log_decorator import log
 
 SERVER_LOGGER = logging.getLogger('server_log')
 
+
 @log
-def process_client_msg(msg):
+def process_client_msg(msg, msg_list, client):
     """
     Обработка сообщений от клиента. На вход получаем сообщение от клиента -
     словарь. Проверяем его корректность и возвращаем ответ клиенту - словарь.
-
     :param msg:
+    :param msg_list:
+    :param client:
     :return:
     """
     if ACTION in msg and msg[ACTION] == PRESENCE and TIME in msg \
             and USER in msg and msg[USER][ACCOUNT_NAME] == 'Guest':
-        return {RESPONSE: 200}
-    return {
-        RESPONSE: 400,
-        ERROR: 'Bad Request'
-    }
+        send_msg(client, {RESPONSE: 200})
+        return
+    elif ACTION in msg and msg[ACTION] == MESSAGE and TIME in msg and \
+            MESSAGE_TEXT in msg:
+        msg_list.append((msg[ACCOUNT_NAME], msg[MESSAGE_TEXT]))
+        return
+    else:
+        send_msg(client, {RESPONSE: 400, ERROR: 'Bad Request'})
+        return
 
 
 def main():
@@ -75,28 +85,60 @@ def main():
                                f'Пример: server.py -p 8079 -a 192.168.1.2')
         sys.exit(1)
 
-    # Активация сокета
+    # список клиентов и список сообщений
+    clients = []
+    messages = []
 
+    # Активация сокета
     transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     transport.bind((listen_address, listen_port))
 
-    # Активация прослушивания порта
+    # Таймаут для операций с сокетом
+    transport.settimeout(0.2)
 
+    # Активация прослушивания порта
     transport.listen(MAX_CONNECTIONS)
 
     while True:
-        client, client_address = transport.accept()
         try:
-            msg_from_client = get_msg(client)
-            SERVER_LOGGER.info(f'Сообщение: {msg_from_client}')
+            # Проверка подключений
+            client, client_address = transport.accept()
+        except OSError as e:
+            pass  # timeout вышел
+        else:
+            SERVER_LOGGER.info(f'Связь установлена: {client_address}')
+            clients.append(client)
             # {'action': 'presence', 'time': 1573760672.167031, 'user': {
             # 'account_name': 'Guest'}}
-            response = process_client_msg(msg_from_client)
-            send_msg(client, response)
-            client.close()
-        except (ValueError, json.JSONDecodeError):
-            SERVER_LOGGER.error('Некорректное сообщение от клиента.')
-            client.close()
+            read_list = []
+            write_list = []
+            try:
+                if clients:
+                    read_list, write_list, e = select.select(clients,
+                                                             clients, [])
+            except OSError as e:
+                pass
+
+            if read_list:
+                for client in read_list:
+                    try:
+                        process_client_msg(get_msg(client), messages, client)
+                    except:
+                        clients.remove(client)
+
+            if messages and write_list:
+                msg = {
+                    ACTION: MESSAGE,
+                    SENDER: messages[0][0],
+                    TIME: time.time(),
+                    MESSAGE_TEXT: messages[0][1]
+                }
+                del messages[0]
+                for client in write_list:
+                    try:
+                        send_msg(client, msg)
+                    except:
+                        clients.remove(client)
 
 
 if __name__ == '__main__':
